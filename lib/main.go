b6555548698c90e88b74d4764f53105fc2b7d833
Lucas Henrique <lucashenrique-teixeira@hotmail.com>
2017-10-29 18:38:44 -0200

    executa function config()

--- lib/main.go
+++ lib/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"github.com/lucashenriqueteixeira/list_go/lib/router"
+	"github.com/lucashenriqueteixeira/list_go/lib/util"
+	
 )
 
 func main() {
+
+	util.ConfigJson()
+
 	router.Routers()
+
 
 }
