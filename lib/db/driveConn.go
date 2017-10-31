7d53193f390c054e53a34a8663091e9fd8ad2d54
Lucas Henrique <lucashenrique-teixeira@hotmail.com>
2017-10-30 23:49:09 -0200

    create function connection database

--- lib/db/driveConn.go
+++ lib/db/driveConn.go
@@ -4,4 +4,26 @@ import (
 	
 	"fmt"
 	"github.com/lucashenriqueteixeira/list_go/lib/util"	
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
+
+func Open() (*sql.DB){
+	host := os.Getenv("HOST")
+	password := os.Getenv("PASSWORD")
+	user := os.Getenv("USER")
+	port := os.Getenv("PORT")
+	name := os.Getenv("NAME")
+
+	access := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",user,password,host,port,name)
+
+	db, err := sql.Open("mysql",access)
+	util.VerifyErr(err)
+
+	//check if connection is active
+	err = db.Ping()
+	util.VerifyErr(err)
+
+	return db
+}
