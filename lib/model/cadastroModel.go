d431a1e5ec18876bd2df96621bf56ddc04373ade
Lucas Henrique <lucashenrique-teixeira@hotmail.com>
2017-10-30 23:36:47 -0200

    melhor organização e dados filtrados

--- lib/model/cadastroModel.go
+++ lib/model/cadastroModel.go
@@ -9,6 +9,11 @@ import (
 	
 )
 
+type Contato struct {
+	Nome string
+	Telefone []string
+}
+
 /*
 * @param Dados map
 * Recebe os dados digitado pelo usuario Filtra e salva no banco
@@ -17,15 +22,40 @@ func Cadastra(Dados url.Values) (error) {
 	
 	Quant := len(Dados)
 	if Quant <= 2 {
-		DadosFiltrados := make(map[string]interface{},Quant)
-		DadosFiltrados["nome"] = template.HTMLEscapeString(Dados["nome"][0])
-		DadosFiltrados["telefone"] = template.HTMLEscapeString(Dados["telefone"][0])
 
-		db.CadastroSave(DadosFiltrados)
+		c := Contato {
+			Nome : Dados.Get("nome"),
+			Telefone : Dados["telefone"],
+		}
 
+	
+		//Gambiarra???? pode ser
+		Result := FiltraDados(&c)
+		Nome := Result.Nome
+		Telefone := Result.Telefone
+		db.CadastroSave(Nome,Telefone)
+		//Fim da gambiarra?
+		
 		return nil
 	}
 
 	//Caso chegue mais de um parametro que pode ser alterado pela url facilmente.
 	return errors.New("Ouve uma alteração proibida nos parametros do formulario")
+}
+
+func FiltraDados(c *Contato) (Contato) {
+
+	C := *c
+	C.Nome = template.HTMLEscapeString(C.Nome)
+	
+	//Pecorre os valores do slice e Escape cada um
+	var DadosTelefone []string
+	for Index, Value := range C.Telefone {
+		DadosTelefone[Index] = template.HTMLEscapeString(Value)
+	}
+
+	C.Telefone = DadosTelefone
+	
+
+	return C
 }
